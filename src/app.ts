54c0d713c6d2c0364954dcb1e2d9ed3bf02407ea
sossw1 <sossw1@gmail.com>
2021-12-17 11:32:08 -0500

    mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
+import mongoose from 'mongoose';
 import express from 'express';
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoURL = 'mongodb://127.0.0.1:27017/taskme-api';
+
+mongoose
+  .connect(mongoURL)
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.log(error.message));
 
 app.listen(PORT, () => {
-  console.log('App running on port: ' + PORT);
+  console.log('App running on port ' + PORT);
 });
