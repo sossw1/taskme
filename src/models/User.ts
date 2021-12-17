099dafa1441f035c95ef5196c2cb708e43cd33ac
sossw1 <sossw1@gmail.com>
2021-12-17 16:13:41 -0500

    name and email strings required

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,15 @@
 import mongoose, { Schema, Model, Document } from 'mongoose';
 
-export const userSchema = new Schema({});
+export const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
 
 const UserDocument: Model<Document> = mongoose.model('User', userSchema);
 export default UserDocument;
