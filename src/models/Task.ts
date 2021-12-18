7999a2c1500c3fbca6830d945bda1be971f69b98
sossw1 <sossw1@gmail.com>
2021-12-18 09:50:44 -0500

    added description and completed fields

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,6 +1,17 @@
 import mongoose, { Schema, Model, Document } from 'mongoose';
 
-export const taskSchema = new Schema({});
+export const taskSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+});
 
 const TaskCollection: Model<Document> = mongoose.model('Task', taskSchema);
 export default TaskCollection;
