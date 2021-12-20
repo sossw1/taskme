d9892223b49ea0bbabb34168e78e2ff55bcbc2cd
sossw1 <sossw1@gmail.com>
2021-12-20 12:37:58 -0500

    Revert "extracted mongoose connection to new file"
    This reverts commit 6c81c357880a1800e7a0726b83a9b243b3c85648.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import mongooseConnect from './db/mongoose';
-import taskRoutes from './routes/api/tasks';
-// modules
+import mongoose from 'mongoose';
 import express from 'express';
 import chalk from 'chalk';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoURL = 'mongodb://127.0.0.1:27017/taskme-api';
 
-app.use(express.json());
-
-mongooseConnect();
-
-taskRoutes(app);
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log('MongoDB ' + chalk.green('connected'));
+  })
+  .catch((error) => {
+    chalk.red(error.message);
+  });
 
 app.listen(PORT, () => {
   console.log('Server running on PORT ' + chalk.yellow(PORT));
