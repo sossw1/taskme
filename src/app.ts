6c81c357880a1800e7a0726b83a9b243b3c85648
sossw1 <sossw1@gmail.com>
2021-12-20 11:42:40 -0500

    extracted mongoose connection to new file

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
-import mongoose from 'mongoose';
+import mongooseConnect from './db/mongoose';
+import taskRoutes from './routes/api/tasks';
+// modules
 import express from 'express';
 import chalk from 'chalk';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoURL = 'mongodb://127.0.0.1:27017/taskme-api';
 
-mongoose
-  .connect(mongoURL)
-  .then(() => {
-    console.log('MongoDB ' + chalk.green('connected'));
-  })
-  .catch((error) => {
-    chalk.red(error.message);
-  });
+app.use(express.json());
+
+mongooseConnect();
+
+taskRoutes(app);
 
 app.listen(PORT, () => {
   console.log('Server running on PORT ' + chalk.yellow(PORT));
