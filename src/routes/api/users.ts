bf17dedf458d5eb63898f2ffac2a8b054794f1b6
sossw1 <sossw1@gmail.com>
2021-12-22 11:13:20 -0500

    route to get user by id

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -19,6 +19,19 @@ export default (app: Express) => {
       });
   });
 
+  app.get('/api/v1/users/:id', (req: Request, res: Response) => {
+    UserCollection.findById(req.params.id)
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((error) => {
+        if (error.name === 'CastError') {
+          return res.status(404).send();
+        }
+        res.status(500).send();
+      });
+  });
+
   app.post('/api/v1/users', (req: Request, res: Response) => {
     const { name, email, password, age } = req.body;
     const user: User = { name, email, password, age };
