83fd3d519bd37c63a9a8f30e27ce285c543c8329
sossw1 <sossw1@gmail.com>
2021-12-23 11:09:55 -0500

    async/await get user by id route

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -18,17 +18,16 @@ export default (app: Express) => {
     }
   });
 
-  app.get('/api/v1/users/:id', (req: Request, res: Response) => {
-    UserCollection.findById(req.params.id)
-      .then((user) => {
-        res.send(user);
-      })
-      .catch((error) => {
-        if (error.name === 'CastError') {
-          return res.status(404).send();
-        }
-        res.status(500).send();
-      });
+  app.get('/api/v1/users/:id', async (req: Request, res: Response) => {
+    try {
+      const user = await UserCollection.findById(req.params.id);
+      res.send(user);
+    } catch (error: any) {
+      if (error.name === 'CastError') {
+        return res.status(404).send();
+      }
+      res.status(500).send();
+    }
   });
 
   app.post('/api/v1/users', async (req: Request, res: Response) => {
