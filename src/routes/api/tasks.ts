6817abd44c93f79aba5488e9cae577edaecd5c7b
sossw1 <sossw1@gmail.com>
2022-01-04 18:47:45 -0500

    refactor to use findById and save instead of findByIdAndUpdate, may use save middleware in future

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -52,32 +52,39 @@ router.post('/api/v1/tasks', async (req: Request, res: Response) => {
 });
 
 router.patch('/api/v1/tasks/:id', async (req: Request, res: Response) => {
-  try {
-    const task = await TaskCollection.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' });
+  } else {
+    try {
+      const task: any = await TaskCollection.findById(req.params.id);
+
+      if (task) {
+        updates.forEach((update) => (task[update] = req.body[update]));
+
+        await task.save();
+
+        res.send(task);
+      } else {
+        return res
+          .status(404)
+          .send({ error: 'Unable to find task with provided ID' });
       }
-    );
-
-    if (!task) {
-      return res
-        .status(404)
-        .send({ error: 'Unable to find task with provided ID' });
+    } catch (error: any) {
+      if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'Invalid task ID' });
+      }
+      if (error.name === 'ValidationError') {
+        const errorMessage = `Invalid task data provided - ${error.errors.description.message}`;
+        return res.status(400).send({ error: errorMessage });
+      }
+      res.sendStatus(500);
     }
-
-    res.send(task);
-  } catch (error: any) {
-    if (error.name === 'CastError') {
-      return res.status(400).send({ error: 'Invalid task ID' });
-    }
-    if (error.name === 'ValidationError') {
-      const errorMessage = `Invalid task data provided - ${error.errors.description.message}`;
-      return res.status(400).send({ error: errorMessage });
-    }
-    res.sendStatus(500);
   }
 });
 
