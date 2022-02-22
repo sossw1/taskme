a22d77cbe1b7ad468e07f4bce01e38eaf52c7960
sossw1 <sossw1@gmail.com>
2022-02-22 15:26:31 -0500

    delete all users and seed before each test

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,13 +1,25 @@
 import app from '../src/app';
 
 import request from 'supertest';
+import UserCollection from '../src/models/User';
+
+const testUser1 = new UserCollection({
+  name: 'Test Name',
+  email: 'testemail@example.com',
+  password: 'testpassword123'
+});
+
+beforeEach(async () => {
+  await UserCollection.deleteMany();
+  await new UserCollection(testUser1).save();
+});
 
 test('Should sign up a new user', async () => {
   await request(app)
     .post('/api/v1/users')
     .send({
-      name: 'Test Name',
-      email: 'testemail@example.com',
+      name: 'Test Name 2',
+      email: 'testemail2@example.com',
       password: 'testpassword123'
     })
     .expect(201);
