af98f76c2c8f506832182f3a07d8bb3cfd2ea736
sossw1 <sossw1@gmail.com>
2022-02-25 11:37:23 -0500

    test created task properties

--- tests/task.test.ts
+++ tests/task.test.ts
@@ -1,7 +1,8 @@
 import app from '../src/app';
+import TaskCollection from '../src/models/Task';
+import { dbSeed, dbClose, testUser1 } from './fixtures/db';
 
 import request from 'supertest';
-import { dbSeed, dbClose, testUser1 } from './fixtures/db';
 
 const testTask1 = {
   description: 'Test Task'
@@ -16,9 +17,17 @@ afterAll(async () => {
 });
 
 test('Should create task for user', async () => {
-  await request(app)
+  const response = await request(app)
     .post('/api/v1/tasks')
     .set('Authorization', `Bearer ${testUser1.tokens[0].token}`)
     .send(testTask1)
     .expect(201);
+
+  const task = await TaskCollection.findById(response.body._id);
+  expect(task).not.toBeNull();
+  if (task) {
+    expect(task.description).toEqual(testTask1.description);
+    expect(task.completed).toEqual(false);
+    expect(task.owner).toEqual(testUser1._id);
+  }
 });
