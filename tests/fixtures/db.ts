e425a4202da8b9774110d867cfe24e71f65bd25a
sossw1 <sossw1@gmail.com>
2022-02-26 12:01:05 -0500

    numbering consistency

--- tests/fixtures/db.ts
+++ tests/fixtures/db.ts
@@ -4,13 +4,27 @@ import TaskCollection from '../../src/models/Task';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
+const user0Id = new mongoose.Types.ObjectId();
 const user1Id = new mongoose.Types.ObjectId();
-const user2Id = new mongoose.Types.ObjectId();
 const task0Id = new mongoose.Types.ObjectId();
 const task1Id = new mongoose.Types.ObjectId();
 const task2Id = new mongoose.Types.ObjectId();
 
 export const users = [
+  {
+    _id: user0Id,
+    name: 'Name 0',
+    email: 'email0@example.com',
+    password: 'password0',
+    tokens: [
+      {
+        token: jwt.sign(
+          { _id: user0Id },
+          process.env.JWT_SECRET || 'd^e#f@a*u$l%t'
+        )
+      }
+    ]
+  },
   {
     _id: user1Id,
     name: 'Name 1',
@@ -24,41 +38,27 @@ export const users = [
         )
       }
     ]
-  },
-  {
-    _id: user2Id,
-    name: 'Name 2',
-    email: 'email2@example.com',
-    password: 'password2',
-    tokens: [
-      {
-        token: jwt.sign(
-          { _id: user2Id },
-          process.env.JWT_SECRET || 'd^e#f@a*u$l%t'
-        )
-      }
-    ]
   }
 ];
 
 export const tasks = [
   {
     _id: task0Id,
-    description: 'Description 1',
+    description: 'Description 0',
     completed: false,
-    owner: user1Id
+    owner: user0Id
   },
   {
     _id: task1Id,
-    description: 'Description 2',
+    description: 'Description 1',
     completed: true,
-    owner: user1Id
+    owner: user0Id
   },
   {
     _id: task2Id,
-    description: 'Description 3',
+    description: 'Description 2',
     completed: true,
-    owner: user2Id
+    owner: user1Id
   }
 ];
 
